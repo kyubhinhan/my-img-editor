fae8346e5754aba61199a69b33c746a4e8deadf3
kyubhinhan <kyubhinhan@gmail.com>
2024-08-01 20:46:51 +0900

    nextUi 도입

---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+    return (
+        <div>
+            <Button>버튼</Button>
+            이미지 업로드 화면입니다.
+        </div>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/upload',
+                permanent: true, // 301 리디렉션 (true로 설정하면 영구 리디렉션)
+            },
+        ];
+    },
+};
 
 export default nextConfig;

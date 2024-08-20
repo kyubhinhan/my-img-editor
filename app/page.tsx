96e15c64cb17d3c4ad876935265f13d34c05f644
kyubhinhan <kyubhinhan@gmail.com>
2024-08-20 21:53:57 +0900

    이미지 업로드 기반 마련

--- app/page.tsx
+++ app/page.tsx
@@ -13,57 +13,61 @@ import ImageDownloadRight from '@/src/imageDownload/imageDownloadRight';
 
 export default function Home() {
   // 전역 데이터 설정
-  const [imageFile, setImageFile] = useState(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [stage, setStage] = useState('upload');
+
   // end of 전역 데이터 설정
 
   // 화면 관련 컴포넌트 설정
-  const [stage, setStage] = useState('upload');
-
-  const imageUploadLeft = ImageUploadLeft();
-  const imageUploadRight = ImageUploadRight({ setStage, nextStage: 'mark' });
-  const imageMarkLeft = ImageMarkLeft();
-  const imageMarkRight = ImageMarkRight({
-    setStage,
-    prevStage: 'upload',
-    nextStage: 'edit',
-  });
-  const imageEditLeft = ImageEditLeft();
-  const imageEditRight = ImageEditRight({
-    setStage,
-    prevStage: 'mark',
-    nextStage: 'download',
-  });
-  const imageDownloadLeft = ImageDownloadLeft();
-  const imageDownloadRight = ImageDownloadRight({
-    setStage,
-    prevStage: 'edit',
-  });
-  const [leftSlot, setLeftSlot] = useState(imageUploadLeft);
-  const [rightSlot, setRightSlot] = useState(imageUploadRight);
-
-  useEffect(() => {
-    if (stage == 'upload') {
-      setLeftSlot(imageUploadLeft);
-      setRightSlot(imageUploadRight);
-    } else if (stage == 'mark') {
-      setLeftSlot(imageMarkLeft);
-      setRightSlot(imageMarkRight);
-    } else if (stage == 'edit') {
-      setLeftSlot(imageEditLeft);
-      setRightSlot(imageEditRight);
-    } else if (stage == 'download') {
-      setLeftSlot(imageDownloadLeft);
-      setRightSlot(imageDownloadRight);
-    } else {
-      // do nothiing 이곳으로 들어오면 안됨
+  const renderLeftSlot = () => {
+    switch (stage) {
+      case 'upload':
+        return (
+          <ImageUploadLeft imageFile={imageFile} setImageFile={setImageFile} />
+        );
+      case 'mark':
+        return <ImageMarkLeft />;
+      case 'edit':
+        return <ImageEditLeft />;
+      case 'download':
+        return <ImageDownloadLeft />;
+      default:
+        return null;
     }
-  }, [stage]);
+  };
+
+  const renderRightSlot = () => {
+    switch (stage) {
+      case 'upload':
+        return <ImageUploadRight setStage={setStage} nextStage="mark" />;
+      case 'mark':
+        return (
+          <ImageMarkRight
+            setStage={setStage}
+            prevStage="upload"
+            nextStage="edit"
+          />
+        );
+      case 'edit':
+        return (
+          <ImageEditRight
+            setStage={setStage}
+            prevStage="mark"
+            nextStage="download"
+          />
+        );
+      case 'download':
+        return <ImageDownloadRight setStage={setStage} prevStage="edit" />;
+      default:
+        return null;
+    }
+  };
 
   // end of 화면 관련 컴포넌트 설정
 
   return (
     <main>
-      <HSplitter left={leftSlot} right={rightSlot} />
+      <HSplitter left={renderLeftSlot()} right={renderRightSlot()} />
     </main>
   );
 }
--- src/imageUpload/imageUploadLeft.tsx
+++ src/imageUpload/imageUploadLeft.tsx
@@ -1,8 +1,14 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, Dispatch, SetStateAction } from 'react';
 
-export default function ImageUploadLeft() {
+export default function ImageUploadLeft({
+  imageFile,
+  setImageFile,
+}: {
+  imageFile: File | null;
+  setImageFile: Dispatch<SetStateAction<File | null>>;
+}) {
   // 이미지 파일 업로드 관련
   const inputFileRef = useRef<HTMLInputElement>(null);
 
@@ -11,18 +17,35 @@ export default function ImageUploadLeft() {
       inputFileRef.current.click();
     }
   };
+
+  const onFileUploaded = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImageFile(file);
+    }
+  };
   // end of 이미지 파일 업로드 관련
 
   return (
     <div className="flex flex-col h-full" style={{ gap: '20px' }}>
       <div className="text-center"> 이미지 업로드</div>
-      <div
-        className="grow bg-stone-500 cursor-pointer"
-        onMouseDown={onMouseDown}
-      >
-        이미지 업로드하거나 보여주는 부분
-        <input ref={inputFileRef} type="file" className="invisible"></input>
-      </div>
+      {imageFile === null ? (
+        <div
+          className="grow bg-stone-500 cursor-pointer"
+          onMouseDown={onMouseDown}
+        >
+          이미지 업로드하거나 보여주는 부분
+          <input
+            ref={inputFileRef}
+            type="file"
+            accept="image/*"
+            className="invisible"
+            onChange={onFileUploaded}
+          />
+        </div>
+      ) : (
+        <div>이미지가 있다</div>
+      )}
       <div className="text-center invisible">이미지 삭제하는 버튼</div>
     </div>
   );
